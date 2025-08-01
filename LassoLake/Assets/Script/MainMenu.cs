8e793c4ed238e057e27692b6285b06fcccc69c07
t. starynski <toristarynski@gmail.com>
2025-08-01 16:28:16 -0400

    exit button

--- LassoLake/Assets/Script/MainMenu.cs
+++ LassoLake/Assets/Script/MainMenu.cs
@@ -12,4 +12,10 @@ public class MainMenu : MonoBehaviour
     {
         gm.LoadLevel("Pens");
     }
+
+    public void ExitGame()
+    {
+        //Debug.Log("Quit"); //to see if it actually works
+        Application.Quit();
+    }
 }
